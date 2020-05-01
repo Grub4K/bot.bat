0274817c396cb3d70948332f84464655de40daec
Grub4K <simonben@abwesend.de>
2020-05-01 17:02:45 +0200

    Fix custom function import
    Import didnt account for module names. This new version does

--- src/functions/__init__.py
+++ src/functions/__init__.py
@@ -1,7 +1,24 @@
-from . import *
+import os
+import inspect
+import pkgutil
+import importlib
 
-functions = {
-    name[4:]: function
-    for name, function in locals().items()
-    if name.startswith('cmd_')
-}
+
+
+# Gather names of all modules
+current_dir = os.path.dirname(__file__)
+module_infos = pkgutil.iter_modules([current_dir])
+module_names = [module_name for _, module_name, _ in module_infos]
+functions = {}
+for module_name in module_names:
+    # do relative import of module
+    module = importlib.import_module('.' + module_name, __name__)
+    # Scan all imported names for a subclass of Domino
+    for object_name in dir(module):
+        obj = getattr(module, object_name)
+        if inspect.isfunction(obj):
+            if object_name.startswith('cmd_'):
+                name = object_name[4:]
+                functions[name] = obj
+
+__ALL__ = ['functions']
